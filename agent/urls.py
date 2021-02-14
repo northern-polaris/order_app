68cc590fd412986105028517440b045dd261457a
Aurora Hoxha <aurorahoxha23@gmail.com>
2021-02-14 14:22:55 +0100

    path name refactor

--- agent/urls.py
+++ agent/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from agent.views import CustomerListCreate, SellerListCreate
 
 urlpatterns = [
-    path('list/', CustomerListCreate.as_view()),
+    path('customer/', CustomerListCreate.as_view()),
     path('seller/', SellerListCreate.as_view()),
 ]
