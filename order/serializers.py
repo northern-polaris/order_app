11641df61f89d965a4ad07e0963a5d0474763cff
Aurora Hoxha <aurorahoxha23@gmail.com>
2021-02-21 22:54:09 +0100

    Seller can create an order with order units

--- order/serializers.py
+++ order/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from order.models import Order, OrderUnit, Counter
@@ -14,4 +15,26 @@ class OrderSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Order
-        fields = ['code', 'code_year', 'customer_id', 'creator_id', 'order_units']
+        fields = ['id', 'customer_id', 'order_units', 'code', 'date_registered', 'code_year', 'creator_id']
+        read_only_fields = ('code', 'id', 'date_registered', 'code_year', 'creator_id')
+
+    def create(self, validated_data):
+        # logged_user = self._kwargs['context']['request'].user
+        order_units = validated_data.pop('order_units')
+
+        logged_user = User.objects.first()
+        code = 1
+        code_year = 2021
+        validated_data['code'] = code
+        validated_data['code_year'] = code_year
+        validated_data['creator_id'] = logged_user
+        order = super().create(validated_data)
+
+        for order_unit in order_units:
+            order_unit['order_id'] = order
+            product_price = order_unit['product_id'].default_price
+            total_price = product_price * order_unit['amount']
+            order_unit['price'] = total_price
+            OrderUnit.objects.create(**order_unit)
+
+        return order
