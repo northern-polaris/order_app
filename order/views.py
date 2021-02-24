0baa0ceb527a496fc60da78f6c455d82b10e679a
Aurora Hoxha <aurorahoxha23@gmail.com>
2021-02-25 00:22:30 +0100

    Refactor

--- order/views.py
+++ order/views.py
@@ -1,33 +1,16 @@
-from collections import OrderedDict
-
-from django.shortcuts import render
-from rest_framework import generics, permissions
+from rest_framework import generics
 from rest_framework.decorators import api_view
-from rest_framework.pagination import PageNumberPagination, BasePagination
-from rest_framework.permissions import BasePermission
 from rest_framework.response import Response
 
 from agent.models import Customer
-from order.models import Order, OrderUnit
-from order.serializers import OrderSerializer, OrderUnitSerializer
-
-# class IsShites(BasePermission):
-#     """
-#     Allows access only to Shites users.
-#     """
-#
-#     def has_permission(self, request, view):
-#         groups = request.user.groups
-#         if "SHites" in groups:
-#             return True
-#         return False
+from order.models import Order
+from order.serializers import OrderSerializer
 from product.models import Product
 
 
 class OrderListCreate(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-    # permissions_classes = (IsShites,)
 
 
 class OrderRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
@@ -35,17 +18,6 @@ class OrderRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = OrderSerializer
 
 
-# class my(PageNumberPagination):
-#
-#     def get_paginated_response(self, data):
-#         return Response(OrderedDict([
-#                 ('count', self.page.paginator.count),
-#                 ('next', self.get_next_link()),
-#                 ('previous', self.get_previous_link()),
-#
-#             ('data', data)
-#         ]))
-#
 @api_view(['GET'])
 def order_form_dependencies(request):
     data = {
