7902398a924ff49e6d85aa827b99f15dfed75095
Aurora Hoxha <aurorahoxha23@gmail.com>
2021-03-07 14:39:19 +0100

    Product category names

--- product/serializers.py
+++ product/serializers.py
@@ -1,12 +1,28 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
 from product.models import Product, ProductCategory
 
 
-class ProductSerializer(serializers.ModelSerializer):
+class ProductReadSerializer(serializers.ModelSerializer):
+    # product_category = SerializerMethodField()
+    product_category_name = SerializerMethodField(read_only=True)
+
     class Meta:
         model = Product
-        fields = ['id', 'name', 'default_price', 'description', 'product_category', 'deleted']
+        fields = ['id', 'name', 'default_price', 'description', 'product_category', 'product_category_name']
+
+    # def get_product_category(self, obj):
+    #     return list(obj.product_category.all().values_list('name', flat=True))
+
+    def get_product_category_name(self, obj):
+        return list(obj.product_category.all().values_list('name', flat=True))
+
+
+class ProductWriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'default_price', 'description', 'product_category']
 
 
 class ProductCategorySerializer(serializers.ModelSerializer):
--- product/views.py
+++ product/views.py
@@ -1,18 +1,35 @@
-from django.shortcuts import render
 from rest_framework import generics
 
 from product.models import Product, ProductCategory
-from product.serializers import ProductSerializer, ProductCategorySerializer
+from product.serializers import ProductCategorySerializer, ProductReadSerializer, \
+    ProductWriteSerializer
 
 
 class ProductListCreate(generics.ListCreateAPIView):
     queryset = Product.objects.filter(deleted=False)
-    serializer_class = ProductSerializer
+    # serializer_class = ProductSerializer
+    read_serializer = ProductReadSerializer
+    write_serializer = ProductWriteSerializer
+
+    def get_serializer_class(self):
+
+        if self.request.method == 'GET':
+            return self.read_serializer
+        else:
+            return self.write_serializer
 
 
 class ProductRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+    read_serializer = ProductReadSerializer
+    write_serializer = ProductWriteSerializer
+
+    def get_serializer_class(self):
+
+        if self.request.method == 'GET':
+            return self.read_serializer
+        else:
+            return self.write_serializer
 
     def perform_destroy(self, instance):
         instance.deleted = True
